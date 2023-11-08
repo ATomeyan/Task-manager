ef05ba2ae7ab050caebb3a99d5f4372201f31f4d
artur.tomeyan <artur.tomeyan@ucom.am>
2023-11-08 17:43:12 +0400

    Implemented file updating method.

--- src/main/java/com/manager/service/TaskFileServiceImpl.java
+++ src/main/java/com/manager/service/TaskFileServiceImpl.java
@@ -18,13 +18,14 @@ public class TaskFileServiceImpl implements TaskFileService {
     public void addTasksToFile(Task task) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("Task.txt", true))) {
 
-            writer.write("\nTask ID: " + task.getId() + "\n" +
+            writer.write("Task ID: " + task.getId() + "\n" +
                     "Title: " + task.getTitle() + "\n" +
                     "Description: " + task.getDescription() + "\n" +
                     "Due date: " + task.getDueDate() + "\n" +
                     "Status: " + task.getStatus() + "\n" +
                     "Task created at: " + new SimpleDateFormat("dd.MM.yyyy HH:mm").format(task.getTaskCreatedAt()));
 
+            writer.newLine();
             writer.newLine();
             writer.flush();
         } catch (IOException e) {
@@ -33,7 +34,28 @@ public class TaskFileServiceImpl implements TaskFileService {
     }
 
     @Override
-    public void updateTaskFile(List<Task> task) {
-        // TODO document why this method is empty
+    public void updateTaskFile(List<Task> tasks) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Task.txt"))) {
+
+            for (Task task : tasks) {
+
+                writer.write("Task ID: " + task.getId() + "\n" +
+                        "Title: " + task.getTitle() + "\n" +
+                        "Description: " + task.getDescription() + "\n" +
+                        "Status: " + task.getStatus() + "\n" +
+                        "Due date: " + task.getDueDate() + "\n" +
+                        "Task created at: " + new SimpleDateFormat("dd.MM.yyyy HH:mm").format(task.getTaskCreatedAt())
+                );
+
+                if (task.getTaskChangedAt() != null)
+                    writer.write("\nTask changed at: " + new SimpleDateFormat("dd.MM.yyyy HH:mm").format(task.getTaskChangedAt()));
+
+                writer.newLine();
+                writer.newLine();
+                writer.flush();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
